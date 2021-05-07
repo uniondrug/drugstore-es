715c894bf8c8b774835af32bbb8e6767e093b246
吕朝晖 <lvchaohui@uniondrug.cn>
2021-05-07 13:53:19 +0800

    1

---
+++ src/EsService.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lvchaohui
+ * Date: 2021/5/7
+ * Time: 1:51 PM
+ */
+namespace Uniondrug\DrugstoreAuth;
+
+use Uniondrug\Framework\Services\Service;
+
+/**
+ * Class DrugstoreAuthService
+ * @package Uniondrug\DrugstoreAuth
+ */
+class EsService extends Service
+{
+
+}
